549eddce46d5bf8394dbc4a98ced7cfb6b766ed6
Nuskit <nuskit@mail.com>
2016-09-07 08:06:45 +0300

    Add testDescription

---
+++ Labs_1_UnitTests/QuickSortTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Labs_1_UnitTests
+{
+  [TestClass]
+  public class QuickSortTest
+  {
+    [TestMethod]
+    public void TestCheckWhenZeroCountElements()
+    {
+    }
+
+    [TestMethod]
+    public void TestSortedArray()
+    {
+    }
+
+    [TestMethod]
+    public void TestReversedSortedArray()
+    {
+    }
+
+    [TestMethod]
+    public void TestDefaultArray()
+    {
+    }
+
+    [TestMethod]
+    public void TestWhenAllEqualValue()
+    {
+    }
+
+    [TestMethod]
+    public void TestWhenArrayHaveNullPointer()
+    {
+    }
+  }
+}
