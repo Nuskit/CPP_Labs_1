274d688c15d439237ce5a55e60a7756dc33a230f
Nuskit <nuskit@mail.com>
2016-09-07 22:51:34 +0300

    Add sorted Interface.

---
+++ Labs_1_QuickSort/ISort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs_1_QuickSort
+{
+  public interface ISort<T>
+  {
+    void Sort(T[] sortedArray, int startPosition, int endPosition, IComparer<T> comparer);
+  }
+}
--- Labs_1_UnitTests/QuickSortTest.cs
+++ Labs_1_UnitTests/QuickSortTest.cs
@@ -1,39 +1,70 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labs_1_QuickSort;
 
 namespace Labs_1_UnitTests
 {
   [TestClass]
   public class QuickSortTest
   {
+    private ISort<int> QuickSortInt
+    {
+      get
+      {
+        return new QuickSort<int>();
+      }
+    }
+
     [TestMethod]
     public void TestCheckWhenZeroCountElements()
     {
+      int[] testingArray = {};
+      QuickSortInt.Sort(testingArray);
+      Assert.AreEqual<int[]>(new int[] {}, );
+    }
+
+    [TestMethod]
+    public void TestWhenNullArray()
+    {
+      int[] testingArray = null;      
+      Assert.IsNull(QuickSortInt.sort(testingArray));
     }
 
     [TestMethod]
     public void TestSortedArray()
     {
+      int[] testingArray = { 0, 1, 2, 3, 4, 5 };
+      Assert.AreEqual<int[]>(new int[] { 0, 1, 2, 3, 4, 5 }, QuickSortInt.sort(testingArray));
     }
 
     [TestMethod]
     public void TestReversedSortedArray()
     {
+      int[] testingArray = {6, 5, 4, 3, 2, 1, 0 };
+      QuickSortInt.sort(testingArray);
+      Assert.AreEqual<int[]>(new int[] { 0, 1, 2, 3, 4, 5, 6 }, testingArray);
     }
 
     [TestMethod]
     public void TestDefaultArray()
     {
+      int[] testingArray = { 7, 2, 4, -3, 15, 0, -10 };
+      QuickSortInt.sort(testingArray);
+      Assert.AreEqual(new int[] { -10, -3 , 0, 2, 4, 7, 15 }, testingArray);
     }
 
     [TestMethod]
     public void TestWhenAllEqualValue()
     {
+      int[] testingArray = { 1, 1, 1, 1, 1, 1 };
+      QuickSortInt.sort(testingArray);
+      Assert.AreEqual(new int[] { 1, 1, 1, 1, 1, 1 }, testingArray);
     }
 
     [TestMethod]
     public void TestWhenArrayHaveNullPointer()
     {
+      
     }
   }
 }
